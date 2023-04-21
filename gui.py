5b3de908e931f9762ab924ffc344e22e9f41cf9b
Issam Silverio Jimenez Ortega <issamjimenez.98@gmail.com>
2023-04-20 17:40:23 -0700

    Boton de agregar

--- gui.py
+++ gui.py
@@ -2,9 +2,23 @@ from modules import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Escribe una nota")
-inputBox = sg.InputText(tooltip="Escriba la nota")
+inputBox = sg.InputText(tooltip="Escriba la nota", key="nota")
 addButton = sg.Button("Agregar")
 
-window = sg.Window("App de Notas", layout=[[label], [inputBox, addButton]])
-window.read()
+window = sg.Window("App de Notas",
+                    layout=[[label], [inputBox, addButton]],
+                    font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    if event == "Agregar":
+            notas = functions.ObtenerNotas()
+            nuevaNota = values['nota'] + "\n"
+            notas.append(nuevaNota)
+            functions.EscribirNotas(notas)
+    elif sg.WIN_CLOSED:
+          break
+
 window.close()
