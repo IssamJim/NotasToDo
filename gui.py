217dec17debe8c0bd722f3e832ed5fb63bf92347
Issam Silverio Jimenez Ortega <issamjimenez.98@gmail.com>
2023-04-26 16:57:16 -0700

    edit button

--- gui.py
+++ gui.py
@@ -4,21 +4,38 @@ import PySimpleGUI as sg
 label = sg.Text("Escribe una nota")
 inputBox = sg.InputText(tooltip="Escriba la nota", key="nota")
 addButton = sg.Button("Agregar")
+listBox = sg.Listbox(values = functions.ObtenerNotas(), key = 'notas',
+                       enable_events=True, size=[45, 10])
+editButton = sg.Button("Editar")
 
 window = sg.Window("App de Notas",
-                    layout=[[label], [inputBox, addButton]],
+                    layout=[[label], [inputBox, addButton],[listBox, editButton]],
                     font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
     print(event)
     print(values)
-    if event == "Agregar":
+
+    match event:
+        case "Agregar":
             notas = functions.ObtenerNotas()
             nuevaNota = values['nota'] + "\n"
             notas.append(nuevaNota)
             functions.EscribirNotas(notas)
-    elif sg.WIN_CLOSED:
-          break
+            window['notas'].update(values=notas)
+        case "Editar":
+            notaEditar = values['notas'][0]
+            nuevaNota = values['nota']
+
+            notas = functions.ObtenerNotas()
+            index = notas.index(notaEditar)
+            notas[index] = nuevaNota
+            functions.EscribirNotas(notas)
+            window['notas'].update(values=notas)
+        case 'notas':
+            window['nota'].update(value=values['notas'][0])
+        case sg.WIN_CLOSED:
+            break
 
 window.close()
