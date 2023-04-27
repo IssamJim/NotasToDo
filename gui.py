9db072e8929d9426112a03e9607739091b88ac7f
Issam Silverio Jimenez Ortega <issamjimenez.98@gmail.com>
2023-04-27 15:22:32 -0700

    cambio de tema y arreglo de bugs en botones edit y complete

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 from modules import functions
 import PySimpleGUI as sg
+import time
 
+sg.theme("BluePurple")
+
+reloj = sg.Text('', key='reloj')
 label = sg.Text("Escribe una nota")
 inputBox = sg.InputText(tooltip="Escriba la nota", key="nota")
 addButton = sg.Button("Agregar")
@@ -11,14 +15,16 @@ completeButton = sg.Button("Completar")
 salirBoton = sg.Button("Salir")
 
 window = sg.Window("App de Notas",
-                    layout=[[label],
+                    layout=[[reloj],
+                            [label],
                             [inputBox, addButton],
                             [listBox, editButton, completeButton],
                             [salirBoton]],
                     font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=10)
+    window["reloj"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(event)
     print(values)
 
@@ -30,23 +36,29 @@ while True:
             functions.EscribirNotas(notas)
             window['notas'].update(values=notas)
         case "Editar":
-            notaEditar = values['notas'][0]
-            nuevaNota = values['nota']
+            try:
+                notaEditar = values['notas'][0]
+                nuevaNota = values['nota']
 
-            notas = functions.ObtenerNotas()
-            index = notas.index(notaEditar)
-            notas[index] = nuevaNota
-            functions.EscribirNotas(notas)
-            window['notas'].update(values=notas)
+                notas = functions.ObtenerNotas()
+                index = notas.index(notaEditar)
+                notas[index] = nuevaNota
+                functions.EscribirNotas(notas)
+                window['notas'].update(values=notas)
+            except IndexError:
+                sg.popup("Seleccione un tarea a editar", font=("Helvetica", 20) )
         case 'notas':
             window['nota'].update(value=values['notas'][0])
         case 'Completar':
-            notaCompleta = values['notas'][0]
-            notas = functions.ObtenerNotas()
-            notas.remove(notaCompleta)
-            functions.EscribirNotas(notas)
-            window['notas'].update(values=notas)
-            window['nota'].update(value='')
+            try:
+                notaCompleta = values['notas'][0]
+                notas = functions.ObtenerNotas()
+                notas.remove(notaCompleta)
+                functions.EscribirNotas(notas)
+                window['notas'].update(values=notas)
+                window['nota'].update(value='')
+            except IndexError:
+                sg.popup("Seleccion una tarea a completar.", font=("Helvetica", 20))
         case 'Salir':
             break
         case sg.WIN_CLOSED:
